271791cdd8af2f3978b5f5c9300c1da813c8a304
Muhammad Fadhil Hidayatullah <103361348+fadhil30@users.noreply.github.com>
2023-04-11 13:38:21 +0700

    Add files via upload

---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xff314728);
+Color grayColor = Color(0xffa9b0a6);
+Color orangeCOlor = Color(0xff9b650);
+Color backgroundColor = Color(0xffe5e5e5);
